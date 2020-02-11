39a27c26c7eff302028de9ca0c5d8db2e7cbae0e
Alejandro Segura <eloso85@gmail.com>
2020-02-10 20:27:29 -0600

    index.js change

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 window.addEventListener('load',() => {
     const sounds = document.querySelectorAll('.sound');
     const pads = document.querySelectorAll('.pads div');
+    const visual = document.querySelector(".visual")
 
     console.log(sounds)
     //make sound
-    pads.forEach((pad, index) => {
+    pads.forEach((pad, index) => { //adding parathenis on pad gives acces to index
         pad.addEventListener('click', function(){
+            sounds[index].currentTime = 0; //set play time to 0 everytime i press
            sounds[index].play();
         });
     });
